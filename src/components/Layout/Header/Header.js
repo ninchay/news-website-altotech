ad956090a00e40fa09da0f21df2c79e19715e5dd
ninchay <csayamchai@gmail.com>
2023-09-01 19:14:58 +0700

    Revert "edit link"
    This reverts commit 84afb58c8c2d40d1f3293a00310be851f506a827.

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,16 +1,12 @@
 import React from 'react'
 import './Header.css'
 import logo from '../../../Assets/Images/the-guardian-logo-white.png'
-import { Link, useNavigate} from 'react-router-dom'
+import { Link} from 'react-router-dom'
 
 function Header({setTypeText}) {
-  const navigate =useNavigate();
+
   const handleTextChange =(e)=>{
     setTypeText(e.target.value);
-    if(e.target.value){
-      setTypeText(e.target.value);
-      navigate(`/searchResult`)
-    }
   };
 
 
@@ -20,7 +16,9 @@ return (
     <Link to='/'><img src={logo} alt='logoPic' className='logoImage'/></Link>
     <div className='search'>
           <div className='inputContainer'>
+            <Link to='/searchResult'>
             <input type="text" className="searchBox" placeholder="Search all news" onChange={handleTextChange}/>
+            </Link>
           </div>
       
     </div>
